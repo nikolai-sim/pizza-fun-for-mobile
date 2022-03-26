cf66b58a3f854683a3091caacecc2dc78e3cfbb5
nikolai-sim <nikolai_sim@hotmail.com>
2022-03-27 08:37:24 +1300

    add test to check that the win updates correctly

--- lib.test.js
+++ lib.test.js
@@ -5,3 +5,20 @@ test('the check function returns pizza when that is the answer' , () => {
   const actual = lib.check('z' , ['p' , 'i' , 'z' , 'z', 'a'] , "pi__a")
   expect(actual).toBe(expected)
 })
+
+test('the update stats function updates the statistic correctly for a win that increses the streak' , () => {
+  const currentStats = {
+    wins : 10,
+    losses : 3,
+    currentStreak: 3,
+    bestStreak: 3,
+  }
+  const expected = {
+    wins : 11,
+    losses : 3,
+    currentStreak: 4,
+    bestStreak: 4,
+  }
+  const actual = lib.updateStats(currentStats, 'win')
+  expect(actual).toEqual(expected)
+})
